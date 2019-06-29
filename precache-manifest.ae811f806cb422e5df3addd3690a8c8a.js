4233fb2b5123ac354b31ba091916844220510eb0
arizh <arish0246@gmail.com>
2019-06-29 23:14:06 +0530

    Updates

---
+++ precache-manifest.ae811f806cb422e5df3addd3690a8c8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8469a9600b68a8f9e772dbce038b7c3",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "a364e76ed7029cab192c",
+    "url": "/calculator/static/css/main.c3b1b7c3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/calculator/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a364e76ed7029cab192c",
+    "url": "/calculator/static/js/main.5cb88c4e.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
